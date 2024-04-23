abbf943f1235fe71b624114ef76e0e349843c91b
cindygomez90 <gomez.cindy@hotmail.com>
2024-04-23 17:14:33 -0300

    Agregando configuración para documentación

---
+++ ecommerce/src/routers/index.js
@@ -0,0 +1,27 @@
+const { Router } = require ("express")
+const viewsRouter = require ("./views.router.js")
+const productsRouter = require ("./products.router.js")
+const cartsRouter = require ("./carts.router.js")
+const messagesRouter = require ("./messages.router.js")
+const sessionsRouter = require("./sessions.router.js")
+const usersRouter = require("./users.router.js")
+const mensajeriaRouter = require("./mensajeria.router.js")
+const pruebasRouter = require("./pruebas.router.js")
+
+const router = Router()
+
+//importación de las rutas
+router.use('/api/products', productsRouter)
+router.use('/api/carts', cartsRouter)
+router.use('/api/messages', messagesRouter)
+router.use('/api/sessions', sessionsRouter)
+router.use('/api/users', usersRouter)
+router.use('/api/mensajeria', mensajeriaRouter)
+router.use('/api/pruebas', pruebasRouter)
+router.use('/', viewsRouter)
+
+router.get('*', (req, res)=>{
+    res.render ('errorpagina')   
+})
+
+module.exports = router
